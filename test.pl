13b8cd695820b272ce2abb746dc482f80b814ad8
kwon-young <kwon-young.choi@hotmail.fr>
2015-09-09 12:25:58 +0200

    add of list.pl containing all utility function for working with list finished tp1 with coloring area

--- test.pl
+++ test.pl
@@ -53,3 +53,167 @@ memeVille(X, Y) :-
 
 enum(X, [T|_], (X,T) ).
 enum(X, [_|Q], R) :- enum(X, Q, R).
+
+%factorial
+
+factorial(0,1).
+
+factorial(X, Y):-
+  X>0,
+  X1 is X-1,
+  factorial(X1, Y1),
+  Y is Y1*X.
+
+factorial2(0, F, F).
+
+factorial2(N, A, F):-
+  N>0,
+  A1 is N*A,
+  N1 is N-1,
+  factorial2(N1, A1, F).
+
+%hanoi puzzle
+
+move(1, X, Y, _):-
+  write('move top disc from '),
+  write(X),
+  write(' to '),
+  write(Y),
+  nl.
+
+move(N, X, Y, Z):-
+  N>1,
+  N1 is N-1,
+  move(N1, X, Z, Y),
+  move(1, X, Y, Z),
+  move(N1, Z, Y, X).
+
+edit(File) :-
+  name(File, FileString),
+  name('gvim ', TextEditString),
+  append(TextEditString, FileString, EDIT),
+  name(E, EDIT),
+  shell(E).
+
+%tree database
+:- op(500, xfx, 'is_parent').
+
+a is_parent b.    c is_parent g.     f is_parent l.     j is_parent q. 
+a is_parent c.    c is_parent h.     f is_parent m.     j is_parent r. 
+a is_parent d.    c is_parent i.     h is_parent n.     j is_parent s. 
+b is_parent e.    d is_parent j.     i is_parent o.     m is_parent t. 
+b is_parent f.    e is_parent k.     i is_parent p. 
+
+%X and Y are siblings
+:- op(500, xfx, 'is_sibling_of').
+
+X is_sibling_of Y :-
+  Z is_parent X,
+  Z is_parent Y,
+  X \== Y.
+
+% X and Y are on the same level in the tree
+:- op(500, xfx, 'is_same_level_as').
+
+X is_same_level_as X.
+X is_same_level_as Y :-
+  W is_parent X,
+  Z is_parent Y,
+  W is_same_level_as Z.
+
+% depth of node in the tree.
+:- op(500, xfx, 'has_depth').
+a has_depth 0 :- !.
+Node has_depth D :-
+  Mother is_parent Node,
+  Mother has_depth D1,
+  D is D1+1.
+
+% locate node by finding a path from root down to the node.
+locate(Node) :-
+  path(Node),
+  write(Node),
+  nl.
+
+path(a).
+path(Node) :-
+  Mother is_parent Node,
+  path(Mother),
+  write(Mother),
+  write(' ---> ').
+
+% calculate the height of a node, length of longest path to a leaf under the node
+
+height(N, H) :-
+  setof(Z, ht(N, Z), Set),
+  max(Set, 0, H).
+
+ht(Node, 0) :-
+  leaf(Node),
+  !.
+ht(Node, H) :-
+  Node is_parent Chid,
+  ht(Child, H1),
+  H is H1+1.
+
+leaf(Node) :-
+  not(is_parent(Node, Child)).
+
+max([], M, M).
+max([X|R], M, A) :-
+  (X > M -> max(R, X, A); max(R, M, A)).
+
+%member definition
+olol(X, [X|_]).
+olol(X, [_|R]) :- olol(X, R).
+
+takeout(X, [X|R], R).
+takeout(X, [F|R], [F|S]) :-
+  takeout(X, R, S).
+
+reverse2([], []).
+reverse2([H|T], R) :-
+  reverse2(T, RevT),
+  append(RevT, [H], R).
+
+perm([X|Y], Z) :-
+  perm(Y, W),
+  takeout(X, Z, W).
+perm([], []).
+
+delete(X, [X|T], T).
+delete(X, [Y|T], [Y|T1]) :-
+  delete(X, T, T1).
+
+% don't understand ????
+prune([], []).
+prune([H|T], B) :-
+  member(H, T),
+  !,
+  prune(T, B).
+prune([H|T], [H|B]) :-
+  prune(T, B).
+
+prefix([], _).
+prefix([H|PT], [H|T]) :-
+  prefix(PT, T).
+
+segment(_, []) :-
+  false.
+segment([H|ST], [H|T]) :-
+  prefix(ST, T),
+  !.
+segment(S, [_|T]) :-
+  segment(S, T).
+
+%change for a dollar
+
+change([H, Q, D, N, P]) :-
+  member(H, [0, 1, 2]),
+  member(Q, [0, 1, 2, 3, 4]),
+  member(D, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+  member(N, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20]),
+  S is 50*H + 25*Q+10*D+5*N,
+  S =< 100,
+  P is 100-S.
--- tp1.pl
+++ tp1.pl
@@ -2,6 +2,10 @@ clear :-
   write('\33\[2J'),
   nl.
 
+% for debug
+% guitracer
+% gtrace
+
 %fait
 sur(a, b).
 sur(b, table).
@@ -26,3 +30,72 @@ puzzle(V1, V2, V3, H1, H2, H3):-
   mot(H1, _, A, _, D, _, G, _),
   mot(H2, _, B, _, E, _, H, _),
   mot(H3, _, C, _, F, _, I, _).
+
+%nth(N, T, L) : T = LN
+nth(1, T, [T|_]).
+nth(N, X, [_|Q]):-
+  nth(N1, X, Q),
+  N is N1 + 1.
+
+% in(T, L) : T E L
+in(T, [T|_]).
+in(T, [_|Q]) :-
+  in(T, Q).
+
+% conc(L1, L2) : L = L1 + L2
+conc([], L, L).
+conc([T|Q], L, [T|QL]) :-
+  conc(Q, L, QL).
+
+%reseau routier
+route(s, a).
+route(s, d).
+route(a, b).
+route(a, d).
+route(b, c).
+route(b, e).
+route(d, e).
+
+voisines(X, Y):-
+  route(X, Y);
+  route(Y, X).
+
+%coloriage
+
+adjacent(X, Y, Map) :-
+  member([X, Y], Map);
+  member([Y, X], Map).
+
+%find_regions([[1,2],[1,3],[1,4],[1,5],[2,3],[2,4],[3,4],[4,5]],[],R).
+
+find_regions([],R,R). 
+find_regions([[X,Y]|S], R,A) :- 
+     (member(X,R) ->  
+        (member(Y,R) -> find_regions(S,R,A) ; find_regions(S,[Y|R],A)) ; 
+           (member(Y,R) -> find_regions(S,[X|R],A) ; find_regions(S,[X,Y|R],A) ) ).
+
+color([[1,2],[1,3],[1,4],[1,5],[2,3],[2,4],[3,4],[4,5]], 
+      [red,green,blue,yellow],Coloring).
+%Coloring = [[5,red],[4,green],[3,red],[1,blue],[2,yellow]] ; ...   [> 48 solutions <] 
+
+color_all([H|T], Colors, [[H, C]|A]) :-
+  member(C, Colors),
+  color_all(T, Colors, A).
+color_all([], _, []).
+
+color(Map, Colors, Result) :-
+  find_regions(Map, [], Regions),
+  color_all(Regions, Colors, Result),
+  \+ conflict(Map, Result).
+
+takeout(X, [X|R], R).
+takeout(X, [F|R], [F|S]) :-
+  takeout(X, R, S).
+
+conflict(_, []).
+conflict(Map, Result) :-
+  member([R1, C], Result),
+  member([R2, C], Result),
+  adjacent(R1, R2, Map),
+  !.
+
